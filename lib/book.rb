39e53ab80c807d46b76dad71cb8638b9554b9497
smh1988-2 <seamhurley1988@gmail.com>
2021-12-14 16:44:42 -0800

    commit tree 0c2cd40ff4604ca0050d308516257bd6682b485c

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,17 @@
+class Book
+    attr_reader :title, :author, :page_count, :genre
+    attr_writer :title, :author, :page_count, :genre
 
+    def initialize(title)
+    @title = title
+    @author = author
+    @book = page_count
+    @genre = genre
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+end
 
